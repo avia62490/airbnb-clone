00e77f7fb6255879a80b9d3e47eeff9704a0906c
Alex Via <alexandervia@gmail.com>
2022-12-15 12:13:32 -0600

    hardcodes experience instance

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,23 @@
 import picture from "../images/KZaferes.png"
+import star from "../images/Star_icon.svg"
+import ellipse from "../images/Ellipse.svg"
+
 export default function Card() {
     return (
         <div className="experiences">
-            <img src={picture} className="experiences--photos" alt=""/>
+              <div className="instance">
+                  <img src={picture} className="instance--photos" alt=""/>
+                  <div className="instance--rating">
+                      <img src={star} alt="" />
+                      <p>5.0 (6)</p>
+                      <img src={ellipse} alt="" />
+                      <p>USA</p>
+                  </div>
+                  <div className="instance--info">
+                      <p>Life lessons with Katie Zaferes</p>
+                      <p><b>From $136 / </b> person</p>
+                  </div>
+              </div>
         </div>
     );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ export default function Header() {
     return (
         <div className="header">
               <nav className="navbar">
-                  <img src={logo} className="logo" alt="airbnb" />
+                  <img src={logo} className="logo" alt="" />
               </nav>
               <div className="header--body">
-                  <img src={collage} className="collage" alt="photos" />
+                  <img src={collage} className="collage" alt="" />
                   <h1>Online Experiences</h1>
                   <p>Join unique interactive activities led by one-of-a-kind hosts, all without leaving home.</p>
               </div>

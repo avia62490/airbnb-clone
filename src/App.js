609d44876b0fd6b4bb555ac047f303cb731f29fe
Alex Via <alexandervia@gmail.com>
2022-12-15 11:55:39 -0600

    adds Card component, hardcoded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header.js"
+import Card from "./components/card.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Card />
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,8 @@
+import picture from "../images/KZaferes.png"
+export default function Card() {
+    return (
+        <div className="experiences">
+            <img src={picture} className="experiences--photos" alt=""/>
+        </div>
+    );
+}

2e69d48b083516b9050a4dc508250aaf5bac2c45
Alex Via <alexandervia@gmail.com>
2022-12-22 15:01:29 -0600

    adds SOLD OUT badge to card image, non-conditionally displayed

--- src/components/card.js
+++ src/components/card.js
@@ -5,12 +5,12 @@ export default function Card(props) {
     const photo = images(`./${props.image}`);
     return (
         <div className="card">
+            <div className="card--badge">SOLD OUT</div>
             <img src={photo} className="card--image" alt="" />
             <div className="card--stats">
                 <img src={star} className="card--star" alt="" />
                 <span>{props.rating}</span>
-                <span>({props.review_count}) • </span>
-                <span>{props.country}</span>
+                <span className="gray">({props.review_count}) • {props.country}</span>
             </div>
             <p className="card--title">{props.title}</p>
             <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
